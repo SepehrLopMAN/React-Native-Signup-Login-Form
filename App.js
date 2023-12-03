7a3ee402129475248afab3f337f3ef6cf926da46
Sepehr <sepehrkatebi61@gmail.com>
2023-12-03 17:44:23 +0400

    Initial commit 0.1 Login functionality added

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  [username, setUsername] = useState("");
+  [usernameError, setUsernameError] = useState(false);
+  [password, setPassword] = useState("");
+  [passowrdError, setPassowrdError] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Login</Text>
+      <TextInput
+        style={[styles.textInp, usernameError ? styles.inputError : {}]}
+        placeholder="Username / Email"
+        value={username}
+        onChangeText={(val) => {
+          setUsername(val);
+          setUsernameError(false);
+        }}
+      />
+      {usernameError && (
+        <Text style={styles.error}> Username/Email required! </Text>
+      )}
+      <TextInput
+        style={[
+          styles.textInp,
+          passowrdError ? styles.inputError : {},
+          !passowrdError ? { marginBottom: 20 } : {},
+        ]}
+        placeholder="Password"
+        value={password}
+        onChangeText={(val) => {
+          setPassword(val);
+          setPassowrdError(false);
+        }}
+        secureTextEntry
+      />
+      {passowrdError && (
+        <Text style={[styles.error, { marginBottom: 20 }]}>
+          {" "}
+          Password required!{" "}
+        </Text>
+      )}
+      <Button
+        title="Submit"
+        onPress={() => {
+          if (password.trim() === "") setPassowrdError(true);
+          if (username.trim() === "") setUsernameError(true);
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    margin: 10,
+  },
+  header: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  textInp: {
+    height: 40,
+    marginTop: 25,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  inputError: {
+    borderColor: "red",
+  },
+  error: {
+    color: "red",
   },
 });
